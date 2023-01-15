18b07f8fe32a421a522b17475f4ad7cad2850e17
kaikai-23 <gate.sea0203@gmail.com>
2023-01-16 08:41:08 +0900

    ✨ ヘッダーの追加

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import classes from './Footer.module.css';
 
 export function Footer() {
   return (
-      <footer>
+      <footer className={classes.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
